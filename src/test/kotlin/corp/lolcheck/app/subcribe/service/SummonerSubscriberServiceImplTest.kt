ae0ce15e3c3ad92d882291ea7d67cb73dc98341a
bandalgomsu <q63530@naver.com>
2024-11-24 12:37:49 +0900

    test : SummonerSubscriberService Test 구현

--- src/test/kotlin/corp/lolcheck/app/subcribe/service/SummonerSubscriberServiceImplTest.kt
+++ src/test/kotlin/corp/lolcheck/app/subcribe/service/SummonerSubscriberServiceImplTest.kt
@@ -1,128 +1,397 @@
-//package corp.lolcheck.app.summoners.service
-//
-//import corp.lolcheck.app.subcribe.SummonerSubscriberTestConst.gameName
-//import corp.lolcheck.app.subcribe.SummonerSubscriberTestConst.subscribeId
-//import corp.lolcheck.app.subcribe.SummonerSubscriberTestConst.summonerId
-//import corp.lolcheck.app.subcribe.SummonerSubscriberTestConst.tagLine
-//import corp.lolcheck.app.subcribe.SummonerSubscriberTestConst.userId
-//import corp.lolcheck.app.subcribe.domain.SummonerSubscriber
-//import corp.lolcheck.app.subcribe.dto.SummonerSubscriberResponse
-//import corp.lolcheck.app.subcribe.exception.SummonerSubscriberErrorCode
-//import corp.lolcheck.app.subcribe.repository.SummonerSubscriberRepository
-//import corp.lolcheck.app.subcribe.service.SummonerSubscriberServiceImpl
-//import corp.lolcheck.app.summoners.domain.Summoner
-//import corp.lolcheck.app.summoners.service.interfaces.SummonerService
-//import corp.lolcheck.common.exception.BusinessException
-//import io.mockk.coEvery
-//import io.mockk.coVerify
-//import io.mockk.junit5.MockKExtension
-//import io.mockk.mockk
-//import io.mockk.spyk
-//import kotlinx.coroutines.test.runTest
-//import org.junit.jupiter.api.Assertions
-//import org.junit.jupiter.api.BeforeEach
-//import org.junit.jupiter.api.DisplayName
-//import org.junit.jupiter.api.Test
-//import org.junit.jupiter.api.extension.ExtendWith
-//import org.mockito.MockitoAnnotations
-//import kotlin.test.assertFailsWith
-//
-//@ExtendWith(MockKExtension::class)
-//class SummonerSubscriberServiceImplTest(
-//) {
-//
-//    var summonerSubscriberRepository: SummonerSubscriberRepository = mockk<SummonerSubscriberRepository>()
-//
-//    var summonerService: SummonerService = mockk<SummonerService>()
-//
-//    var summonerSubscriberService: SummonerSubscriberServiceImpl =
-//        spyk(SummonerSubscriberServiceImpl(summonerSubscriberRepository, summonerService))
-//
-//    val summoner: Summoner = Summoner(
-//        id = summonerId,
-//        puuid = "test",
-//        gameName = gameName,
-//        tagLine = tagLine,
-//    )
-//
-//    val summonerSubscriber: SummonerSubscriber = SummonerSubscriber(
-//        id = subscribeId,
-//        summonerId = summonerId,
-//        subscriberId = userId,
-//    )
-//
-//    @BeforeEach
-//    fun beforeEach() {
-//        MockitoAnnotations.openMocks(this)
-//    }
-//
-//    @Test
-//    @DisplayName("소환사_구독_성공_테스트")
-//    fun 소환사_구독_성공_테스트() = runTest {
-//        coEvery { summonerService.getSummonerById(summonerId) } returns summoner
-//
-//        coEvery { summonerSubscriberRepository.findBySubscriberIdAndSummonerId(userId, summonerId) } returns null
-//
-//        coEvery { summonerSubscriberRepository.save(any()) } returns summonerSubscriber
-//
-//        val response: SummonerSubscriberResponse.SummonerSubscriberInfo =
-//            summonerSubscriberService.subscribeSummoner(userId, summonerId)
-//
-//        Assertions.assertEquals(userId, response.subscriberId)
-//        Assertions.assertEquals(summonerId, response.summonerId)
-//    }
-//
-//    @Test
-//    @DisplayName("소환사_구독_실패_테스트_존재하지 않는 소환사")
-//    fun 소환사_구독_실패_테스트_중복_구독() = runTest {
-//        coEvery { summonerService.getSummonerById(summonerId) } returns summoner
-//        coEvery {
-//            summonerSubscriberRepository.findBySubscriberIdAndSummonerId(
-//                userId,
-//                summonerId
-//            )
-//        } returns summonerSubscriber
-//
-//        assertFailsWith<BusinessException> {
-//            summonerSubscriberService.subscribeSummoner(
-//                userId,
-//                summonerId
-//            )
-//        }
-//    }
-//
-//    @Test
-//    @DisplayName("소환사_구독취소_성공_테스트")
-//    fun 소환사_구독취소_성공_테스트() = runTest {
-//        coEvery {
-//            summonerSubscriberRepository.findBySubscriberIdAndSummonerId(
-//                userId,
-//                summonerId
-//            )
-//        } returns summonerSubscriber
-//
-//        coEvery { summonerSubscriberRepository.delete(summonerSubscriber) } returns Unit
-//
-//        summonerSubscriberService.unsubscribeSummoner(userId, summonerId)
-//
-//        coVerify(exactly = 1) { summonerSubscriberRepository.delete(summonerSubscriber) }
-//    }
-//
-//    @Test
-//    @DisplayName("소환사_구독취소_실패_테스트_존재하지_않는_구독")
-//    fun 소환사_구독취소_실패_테스트_존재하지_않는_구독() = runTest {
-//        coEvery {
-//            summonerSubscriberRepository.findBySubscriberIdAndSummonerId(
-//                userId,
-//                summonerId
-//            )
-//        } throws BusinessException(SummonerSubscriberErrorCode.SUMMONER_SUBSCRIBER_NOT_FOUND)
-//
-//        assertFailsWith<BusinessException> {
-//            summonerSubscriberService.unsubscribeSummoner(
-//                userId,
-//                summonerId
-//            )
-//        }
-//    }
-//}
+package corp.lolcheck.app.summoners.service
+
+import corp.lolcheck.app.subcribe.SummonerSubscriberTestConst.userId
+import corp.lolcheck.app.subcribe.domain.SummonerSubscriber
+import corp.lolcheck.app.subcribe.dto.SummonerSubscriberResponse
+import corp.lolcheck.app.subcribe.exception.SummonerSubscriberErrorCode
+import corp.lolcheck.app.subcribe.repository.SummonerSubscriberRepository
+import corp.lolcheck.app.subcribe.service.SummonerSubscriberServiceImpl
+import corp.lolcheck.app.summoners.domain.Summoner
+import corp.lolcheck.app.summoners.service.interfaces.SummonerService
+import corp.lolcheck.common.exception.BusinessException
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
+
+class SummonerSubscriberServiceImplTest(
+) {
+
+    private var summonerSubscriberRepository: SummonerSubscriberRepository = mockk<SummonerSubscriberRepository>()
+
+    private var summonerService: SummonerService = mockk<SummonerService>()
+
+    private var summonerSubscriberService: SummonerSubscriberServiceImpl =
+        SummonerSubscriberServiceImpl(
+            summonerService = summonerService,
+            summonerSubscriberRepository = summonerSubscriberRepository
+        )
+
+    @Test
+    @DisplayName("SUBSCRIBE_SUMMONER_SUCCESS")
+    fun SUBSCRIBE_SUMMONER_SUCCESS() = runTest {
+        val subscriberId = 1L
+        val summonerId = 1L
+
+        val summoner = Summoner(
+            id = summonerId,
+            puuid = "TEST_PUUID",
+            gameName = "TEST_GAME_NAME",
+            tagLine = "TEST_TAG_LINE",
+            introduce = "TEST_INTRODUCE",
+            recentGameId = 2L
+        )
+
+        val summonerSubscriber = SummonerSubscriber(
+            id = 1L,
+            summonerId = summonerId,
+            subscriberId = subscriberId
+        )
+
+        coEvery { summonerSubscriberRepository.findAllBySubscriberId(subscriberId) } returns flow {
+            emit(
+                summonerSubscriber
+            )
+        }
+
+        coEvery { summonerService.getSummonerById(summonerId) } returns summoner
+
+        coEvery { summonerSubscriberRepository.findBySubscriberIdAndSummonerId(subscriberId, summonerId) } returns null
+
+        coEvery { summonerSubscriberRepository.save(any()) } returns summonerSubscriber
+
+        val response: SummonerSubscriberResponse.SummonerSubscriberInfo =
+            summonerSubscriberService.subscribeSummoner(userId, summonerId)
+
+        assertEquals(1L, response.id)
+        assertEquals(userId, response.subscriberId)
+        assertEquals(summonerId, response.summonerId)
+        assertEquals("TEST_GAME_NAME", response.summonerGameName)
+        assertEquals("TEST_TAG_LINE", response.summonerTagLine)
+        assertEquals("TEST_INTRODUCE", response.summonerIntroduce)
+    }
+
+    @Test
+    @DisplayName("SUBSCRIBE_SUMMONER_FAILURE_THROW_BY_MAX_COUNT_SUBSCRIBE")
+    fun SUBSCRIBE_SUMMONER_FAILURE_THROW_BY_MAX_COUNT_SUBSCRIBE() = runTest {
+        val subscriberId = 1L
+        val summonerId = 1L
+
+        coEvery { summonerSubscriberRepository.findAllBySubscriberId(subscriberId) } returns flow {
+            emit(
+                SummonerSubscriber(
+                    id = 1L,
+                    summonerId = summonerId,
+                    subscriberId = subscriberId
+                )
+            )
+            emit(
+                SummonerSubscriber(
+                    id = 1L,
+                    summonerId = summonerId,
+                    subscriberId = subscriberId
+                )
+            )
+            emit(
+                SummonerSubscriber(
+                    id = 1L,
+                    summonerId = summonerId,
+                    subscriberId = subscriberId
+                )
+            )
+        }
+
+        val exception = assertThrows<BusinessException> {
+            summonerSubscriberService.subscribeSummoner(userId, summonerId)
+        }
+
+        assertEquals(SummonerSubscriberErrorCode.MAX_COUNT_SUBSCRIBE.code, exception.errorCode.getCodeValue())
+        assertEquals(SummonerSubscriberErrorCode.MAX_COUNT_SUBSCRIBE.message, exception.errorCode.getMessageValue())
+        assertEquals(SummonerSubscriberErrorCode.MAX_COUNT_SUBSCRIBE.status, exception.errorCode.getStatusValue())
+    }
+
+    @Test
+    @DisplayName("SUBSCRIBE_SUMMONER_FAILURE_THROW_BY_DUPLICATE_SUMMONER_SUBSCRIBER")
+    fun SUBSCRIBE_SUMMONER_FAILURE_THROW_BY_DUPLICATE_SUMMONER_SUBSCRIBER() = runTest {
+        val subscriberId = 1L
+        val summonerId = 1L
+
+        val summoner = Summoner(
+            id = summonerId,
+            puuid = "TEST_PUUID",
+            gameName = "TEST_GAME_NAME",
+            tagLine = "TEST_TAG_LINE",
+            introduce = "TEST_INTRODUCE",
+            recentGameId = 2L
+        )
+
+        val summonerSubscriber = SummonerSubscriber(
+            id = 1L,
+            summonerId = summonerId,
+            subscriberId = subscriberId
+        )
+
+        coEvery { summonerSubscriberRepository.findAllBySubscriberId(subscriberId) } returns flow {
+            emit(
+                SummonerSubscriber(
+                    id = 1L,
+                    summonerId = summonerId,
+                    subscriberId = subscriberId
+                )
+            )
+        }
+
+        coEvery { summonerService.getSummonerById(summonerId) } returns summoner
+
+        coEvery {
+            summonerSubscriberRepository.findBySubscriberIdAndSummonerId(
+                subscriberId,
+                summonerId
+            )
+        } returns summonerSubscriber
+
+        val exception = assertThrows<BusinessException> {
+            summonerSubscriberService.subscribeSummoner(userId, summonerId)
+        }
+
+        assertEquals(
+            SummonerSubscriberErrorCode.DUPLICATE_SUMMONER_SUBSCRIBER.code,
+            exception.errorCode.getCodeValue()
+        )
+        assertEquals(
+            SummonerSubscriberErrorCode.DUPLICATE_SUMMONER_SUBSCRIBER.message,
+            exception.errorCode.getMessageValue()
+        )
+        assertEquals(
+            SummonerSubscriberErrorCode.DUPLICATE_SUMMONER_SUBSCRIBER.status,
+            exception.errorCode.getStatusValue()
+        )
+    }
+
+    @Test
+    @DisplayName("GET_MY_SUBSCRIBES_SUCCESS")
+    fun GET_MY_SUBSCRIBES_SUCCESS() = runTest {
+        val subscriberId = 1L
+        val summonerId = 1L
+
+        val summoner = Summoner(
+            id = summonerId,
+            puuid = "TEST_PUUID",
+            gameName = "TEST_GAME_NAME",
+            tagLine = "TEST_TAG_LINE",
+            introduce = "TEST_INTRODUCE",
+            recentGameId = 2L
+        )
+
+        val summonerSubscriber = SummonerSubscriber(
+            id = 1L,
+            summonerId = summonerId,
+            subscriberId = subscriberId
+        )
+
+        coEvery { summonerSubscriberRepository.findAllBySubscriberId(userId) } returns flow { emit(summonerSubscriber) }
+        coEvery { summonerService.getSummonerById(summonerId) } returns summoner
+
+        val response = summonerSubscriberService.getMySubscribes(userId).toList()
+
+        assertEquals(1, response.size)
+        assertEquals(1L, response[0].id)
+        assertEquals(subscriberId, response[0].subscriberId)
+        assertEquals(summonerId, response[0].summonerId)
+        assertEquals("TEST_GAME_NAME", response[0].summonerGameName)
+        assertEquals("TEST_TAG_LINE", response[0].summonerTagLine)
+        assertEquals("TEST_INTRODUCE", response[0].summonerIntroduce)
+    }
+
+    @Test
+    @DisplayName("GET_SUBSCRIBER_IDS_BY_SUMMONER_IDS")
+    fun GET_SUBSCRIBER_IDS_BY_SUMMONER_IDS() = runTest {
+        val subscriberId = 1L
+        val summonerId = 1L
+
+        val summonerSubscriber = SummonerSubscriber(
+            id = 1L,
+            summonerId = summonerId,
+            subscriberId = subscriberId
+        )
+
+        val summonerIds = listOf(summonerId)
+
+        coEvery { summonerSubscriberRepository.findAllBySubscriberIdIn(summonerIds) } returns flow {
+            emit(
+                summonerSubscriber
+            )
+        }
+
+        val response = summonerSubscriberService.getSubscriberIdsBySummonerIds(summonerIds)
+
+        assertEquals(1, response.size)
+        assertEquals(subscriberId, response[0])
+    }
+
+    @Test
+    @DisplayName("GET_MY_SUBSCRIBE_SUCCESS")
+    fun GET_MY_SUBSCRIBE_SUCCESS() = runTest {
+        val subscriberId = 1L
+        val summonerId = 1L
+
+        val summoner = Summoner(
+            id = summonerId,
+            puuid = "TEST_PUUID",
+            gameName = "TEST_GAME_NAME",
+            tagLine = "TEST_TAG_LINE",
+            introduce = "TEST_INTRODUCE",
+            recentGameId = 2L
+        )
+
+        val summonerSubscriber = SummonerSubscriber(
+            id = 1L,
+            summonerId = summonerId,
+            subscriberId = subscriberId
+        )
+
+        coEvery {
+            summonerSubscriberRepository.findBySubscriberIdAndSummonerId(
+                userId,
+                summonerId
+            )
+        } returns summonerSubscriber
+        coEvery { summonerService.getSummonerById(summonerId) } returns summoner
+
+        val response = summonerSubscriberService.getMySubscribe(userId, summonerId)
+
+        assertEquals(1L, response.id)
+        assertEquals(subscriberId, response.subscriberId)
+        assertEquals(summonerId, response.summonerId)
+        assertEquals("TEST_GAME_NAME", response.summonerGameName)
+        assertEquals("TEST_TAG_LINE", response.summonerTagLine)
+        assertEquals("TEST_INTRODUCE", response.summonerIntroduce)
+    }
+
+    @Test
+    @DisplayName("GET_MY_SUBSCRIBE_FAILURE_THROW_BY_SUMMONER_SUBSCRIBER_NOT_FOUND")
+    fun GET_MY_SUBSCRIBE_FAILURE_THROW_BY_SUMMONER_SUBSCRIBER_NOT_FOUND() = runTest {
+        val subscriberId = 1L
+        val summonerId = 1L
+
+        coEvery {
+            summonerSubscriberRepository.findBySubscriberIdAndSummonerId(
+                userId,
+                summonerId
+            )
+        } returns null
+
+        val exception =
+            assertThrows<BusinessException> { summonerSubscriberService.getMySubscribe(userId, summonerId) }
+
+        assertEquals(
+            SummonerSubscriberErrorCode.SUMMONER_SUBSCRIBER_NOT_FOUND.code,
+            exception.errorCode.getCodeValue()
+        )
+        assertEquals(
+            SummonerSubscriberErrorCode.SUMMONER_SUBSCRIBER_NOT_FOUND.message,
+            exception.errorCode.getMessageValue()
+        )
+        assertEquals(
+            SummonerSubscriberErrorCode.SUMMONER_SUBSCRIBER_NOT_FOUND.status,
+            exception.errorCode.getStatusValue()
+        )
+    }
+
+    @Test
+    @DisplayName("GET_SUBSCRIBER_BY_SUMMONER_ID")
+    fun GET_SUBSCRIBER_BY_SUMMONER_ID() = runTest {
+        val subscriberId = 1L
+        val summonerId = 1L
+
+        val summoner = Summoner(
+            id = summonerId,
+            puuid = "TEST_PUUID",
+            gameName = "TEST_GAME_NAME",
+            tagLine = "TEST_TAG_LINE",
+            introduce = "TEST_INTRODUCE",
+            recentGameId = 2L
+        )
+
+        val summonerSubscriber = SummonerSubscriber(
+            id = 1L,
+            summonerId = summonerId,
+            subscriberId = subscriberId
+        )
+
+        coEvery { summonerSubscriberRepository.findAllBySummonerId(summonerId) } returns flow { emit(summonerSubscriber) }
+        coEvery { summonerService.getSummonerById(summonerId) } returns summoner
+
+
+        val response = summonerSubscriberService.getSubscriberBySummonerId(summonerId)
+
+        assertEquals(1, response.size)
+        assertEquals(1L, response[0].id)
+        assertEquals(subscriberId, response[0].subscriberId)
+        assertEquals(summonerId, response[0].summonerId)
+        assertEquals("TEST_GAME_NAME", response[0].summonerGameName)
+        assertEquals("TEST_TAG_LINE", response[0].summonerTagLine)
+        assertEquals("TEST_INTRODUCE", response[0].summonerIntroduce)
+    }
+
+    @Test
+    @DisplayName("UNSUBSCRIBE_SUCCESS")
+    fun UNSUBSCRIBE_SUCCESS() = runTest {
+        val subscriberId = 1L
+        val summonerId = 1L
+
+        val summonerSubscriber = SummonerSubscriber(
+            id = 1L,
+            summonerId = summonerId,
+            subscriberId = subscriberId
+        )
+
+        coEvery {
+            summonerSubscriberRepository.findBySubscriberIdAndSummonerId(
+                subscriberId,
+                summonerId
+            )
+        } returns summonerSubscriber
+        coEvery { summonerSubscriberRepository.delete(summonerSubscriber) } returns Unit
+
+        summonerSubscriberService.unsubscribeSummoner(subscriberId, summonerId)
+
+        coVerify(exactly = 1) { summonerSubscriberRepository.delete(summonerSubscriber) }
+    }
+
+    @Test
+    @DisplayName("UNSUBSCRIBE_FAILURE_THROW_BY_SUMMONER_SUBSCRIBER_NOT_FOUND")
+    fun UNSUBSCRIBE_FAILURE_THROW_BY_SUMMONER_SUBSCRIBER_NOT_FOUND() = runTest {
+        val subscriberId = 1L
+        val summonerId = 1L
+
+        coEvery {
+            summonerSubscriberRepository.findBySubscriberIdAndSummonerId(
+                subscriberId,
+                summonerId
+            )
+        } returns null
+
+        val exception =
+            assertThrows<BusinessException> { summonerSubscriberService.unsubscribeSummoner(subscriberId, summonerId) }
+
+        assertEquals(
+            SummonerSubscriberErrorCode.SUMMONER_SUBSCRIBER_NOT_FOUND.code,
+            exception.errorCode.getCodeValue()
+        )
+        assertEquals(
+            SummonerSubscriberErrorCode.SUMMONER_SUBSCRIBER_NOT_FOUND.message,
+            exception.errorCode.getMessageValue()
+        )
+        assertEquals(
+            SummonerSubscriberErrorCode.SUMMONER_SUBSCRIBER_NOT_FOUND.status,
+            exception.errorCode.getStatusValue()
+        )
+    }
+}
